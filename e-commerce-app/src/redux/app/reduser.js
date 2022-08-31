08782093b020530f2afc2a4f2509cf80e0122ccd
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-08-31 16:01:42 +0530

    structure Build

---
+++ e-commerce-app/src/redux/app/reduser.js
@@ -0,0 +1,15 @@
+
+const initState = {
+   data:[],
+   loading:false,
+   error:false
+    
+  };
+  
+  export const appReduser = (state=initState, { type, payload }) => {
+    switch (type) {
+      
+      default:
+        return state;
+    }
+  };
